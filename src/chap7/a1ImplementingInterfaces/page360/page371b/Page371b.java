35d41df633f5cd966d5f8ce0becd757a993e6fb1
sheryldreyer <sheryldreyer2@gmail.com>
2026-03-24 12:48:52 +0200

    committing chap 7

---
+++ src/chap7/a1ImplementingInterfaces/page360/page371b/Page371b.java
@@ -0,0 +1,33 @@
+package chap7.a1ImplementingInterfaces.page360.page371b;
+
+//Declaring static Interface Methods
+//Interfaces can also include static methods.
+// These methods are defined explicitly with the static keyword and, for the most part,
+// behave just like static methods defined in classes.
+//Static Interface Method Definition Rules
+
+//A static method must be marked with the static keyword and include a method body.
+//A static method without an access modifier is implicitly public.
+//A static method cannot be marked abstract or final.
+//A static method is not inherited and cannot be accessed in a class implementing the interface without
+// a reference to the interface name.
+
+//These rules should follow from what you know so far of classes, interfaces, and static methods.
+//For example, you can’t declare static methods without a body in classes, either.
+//Like default and abstract interface methods, static interface methods are implicitly public
+// if they are declared without an access modifier. As you see shortly, you can use the private access modifier with static methods.
+//Let’s take a look at a static interface method:
+ interface Hop {
+    static int getJumpHeight() {
+        return 8;
+    }
+}
+
+//Since the method is defined without an access modifier, the compiler will automatically insert the public access modifier. The method getJumpHeight() works just like a static method as defined in a class. In other words, it can be accessed without an instance of a class.
+  class Skip implements Hop {
+    public int skip() {
+        return Hop.getJumpHeight();
+    }
+}
+//Notice we don’t have the same problem we did when we inherited two default interface methods with the same signature.
+// Java “solved” the multiple inheritance problem of static interface methods by not allowing them to be inherited!
---
+++ src/chap7/a1ImplementingInterfaces/page360/page372.java
@@ -0,0 +1,36 @@
+package chap7.a1ImplementingInterfaces.page360;
+
+//Reusing Code with private Interface Methods
+//The last two types of concrete methods that can be added to interfaces are private and private
+// static interface methods. Because both types of methods are private,
+// they can only be used in the interface declaration in which they are declared.
+// For this reason, they were added primarily to reduce code duplication.
+// For example, consider the following code sample:
+interface Schedule {
+    default void wakeUp()        { checkTime(7);  }
+    private static void haveBreakfast() { checkTime(9);  }
+    static void workOut()        { checkTime(18); }
+    private static void checkTime(int hour) {
+        if (hour > 17) {
+
+            System.out.println("You’re late!");
+        } else {
+            System.out.println("You have "+(17-hour)+" hours left "
+                    + "to make the appointment");
+        }
+    }
+}
+
+//You could write this interface without using a private method by copying the contents of the checkTime()
+// method into the places it is used. It’s a lot shorter and easier to read if you don’t!
+// Since the authors of Java were nice enough to add this feature for our convenience,
+// we might as well use it!
+
+//We could have also declared checkTime() as public in the previous example,
+// but this would expose the method to use outside the interface.
+// One important tenet of encapsulation is to not expose the internal workings of a
+// class or interface when not required. We cover encapsulation later in this chapter.
+
+//The difference between a non-static private method and a static one is
+// analogous to the difference between an instance and static method declared within a class.
+// In particular, it’s all about what methods each can be called from.
