726c6150838f7c0aacd2dd5f4842f767a8cafc82
sheryldreyer <sheryldreyer2@gmail.com>
2026-03-26 18:19:32 +0200

    committing chap 9

--- src/chap9/a1UsingCommonCollectionAPIs/page488/Page488.java
+++ src/chap9/a1UsingCommonCollectionAPIs/page488/Page488.java
@@ -1,52 +1,65 @@
 package chap9.a1UsingCommonCollectionAPIs.page488;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Page488 {
     public static void main(String[] args) {
-//        Removing with Conditions
-//        The removeIf() method removes all elements that match a condition.
-//        We can specify what should be deleted using a block of code or even a method reference.
-//        The method signature looks like the following.
-//        (We explain what the ? super means in the “Working with Generics” section later in this chapter.)
-//        public boolean removeIf (Predicate < ? super E > filter)
+//        Other Iteration Approaches
 
-//        It uses a Predicate, which takes one parameter and returns a boolean.
-//        Let’s take a look at an example:
-        Collection<String> list = new ArrayList<>();
-        list.add("Magician");
-        list.add("Assistant");
-        System.out.println(list);     // [Magician, Assistant]
-        list.removeIf(s -> s.startsWith("A"));
-        System.out.println(list);     // [Magician]
+//        There are other ways to iterate through a Collection.
+//        For example, in Chapter 3, “Making Decisions,” you saw how to loop through a
+//        list using an enhanced for loop.
+        Collection<String> coll = new ArrayList<>();
+        coll.add("Magician");
+        coll.add("Assistant");
+        for (String element : coll)
+            System.out.println(element);
 
-//        Line 8 shows how to remove all of the String values that begin with the letter A.
-//        This allows us to make the Assistant disappear. Let’s try an example with a method reference:
-        Collection<String> set = new HashSet<>();
-        set.add("Wand");
-        set.add("");
-        set.removeIf(String::isEmpty); // s -> s.isEmpty()
-        System.out.println(set);       // [Wand]
+//        You may see another older approach used.
 
-//        On line 14, we remove any empty String objects from set.
-//        The comment on that line shows the lambda equivalent of the method reference.
-//        Line 15 shows that the removeIf() method successfully removed one element from list.
+        Iterator<String> iter = coll.iterator();
+        while (iter.hasNext()) {
+            String name = iter.next();
+            System.out.println(name);
+        }
 
+//        Pay attention to the difference between these techniques.
+//        The hasNext() method checks whether there is a next value.
+//        In other words, it tells you whether next() will execute without throwing an exception.
+//        The next() method actually moves the Iterator to the next element.
 
-//        Iterating on a Collection
-//        There’s a forEach() method that you can call on a Collection instead of writing a loop.
-//        It uses a Consumer that takes a single parameter and doesn’t return anything.
-//        The method signature is as follows:
-//        public void forEach(Consumer<? super T> action)
+//        Determining Equality
+//        There is a custom implementation of equals() so you can compare two Collections
+//        to compare the type and contents. The implementation will vary.
+//        For example, ArrayList checks order, while HashSet does not.
+//        boolean equals (Object object)
 
-//        Cats like to explore, so let’s print out two of them using both method references and lambdas:
-        Collection<String> cats = List.of("Annie", "Ripley");
-        cats.forEach(System.out::println);
-        cats.forEach(c -> System.out.println(c));
+//        The following shows an example:
+        var list1 = List.of(1, 2);
+        var list2 = List.of(2, 1);
+        var set1 = Set.of(1, 2);
+        var set2 = Set.of(2, 1);
 
-//        The cats have discovered how to print their names. Now they have more time to play (as do we)!
+        System.out.println(list1.equals(list2));  // false
+        System.out.println(set1.equals(set2));    // true
+        System.out.println(list1.equals(set1));   // false
+
+//        Line 28 prints false because the elements are in a different order, and a List cares about order.
+//        By contrast, line 29 prints true because a Set is not sensitive to order.
+//        Finally, line 30 prints false because the types are different.
+
+//                Unboxing nulls
+
+//        Java protects us from many problems with Collections.
+//        However, it is still possible to write a NullPointerException.
+
+        var heights = new ArrayList<Integer>();
+        heights.add(null);
+        int h = heights.get(0);  // NullPointerException
+
+//        On line 4, we add a null to the list.
+//        This is legal because a null reference can be assigned to any reference variable.
+//        On line 5, we try to unbox that null to an int primitive.
+//        This is a problem. Java tries to get the int value of null. Since calling any method on null gives a NullPointerException, that is just what we get. Be careful when you see null in relation to autoboxing.
     }
 }
